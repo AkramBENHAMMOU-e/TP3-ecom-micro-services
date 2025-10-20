f7a1741cf6471622db1d612a7e1b34a8f94555cb
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-20 17:28:27 +0100

    initialisation de inventory service et tester avec repository rest

---
+++ inventory-service/src/main/java/com/tp/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,32 @@
+package com.tp.inventoryservice;
+
+import com.tp.inventoryservice.entities.Product;
+import com.tp.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ProductRepository productRepository){
+
+        return args -> {
+        	productRepository.save(Product.builder()
+					.id(UUID.randomUUID().toString()).name("computer").price(122)
+					.quantity(22).build());
+			productRepository.save(Product.builder()
+					.id(UUID.randomUUID().toString()).name("printer").price(333)
+					.quantity(19).build());
+        };
+    }
+
+}
---
+++ inventory-service/src/main/java/com/tp/inventoryservice/entities/Product.java
@@ -0,0 +1,14 @@
+package com.tp.inventoryservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity @AllArgsConstructor @NoArgsConstructor @Builder @Getter @Setter @ToString
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+}
