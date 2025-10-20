a560f06bcdb8827f5c5ba23c39ba72d23c5b565d
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-20 16:33:59 +0100

    test customer service with repository rest

--- customer-service/src/main/java/com/tp/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/tp/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,35 @@
 package com.tp.customerservice;
 
+import com.tp.customerservice.entities.Customer;
+import com.tp.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder()
+                            .name("ahmed").email("ahmed@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("aya").email("aya@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("ali").email("ali@gmail.com")
+                    .build());
+
+            customerRepository.findAll().forEach(System.out::println);
+
+        };
     }
 
 }
