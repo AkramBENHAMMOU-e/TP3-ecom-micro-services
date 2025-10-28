1bf6b1295f3388964a4fcbabac8831ffe2aa08d3
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-28 14:32:57 +0100

    centralisation de la config des microservices

--- customer-service/src/main/java/com/tp/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/tp/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.tp.customerservice;
 
+import com.tp.customerservice.config.CustomerConfigParams;
 import com.tp.customerservice.entities.Customer;
 import com.tp.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
